a86c698e81ad498c7a9610a231851fe6f6c7a681
sapozol <sebastian.pozol14@gmail.com>
2026-04-14 03:11:12 -0500

    SPL Project version 1.2.4

--- config/dev.exs
+++ config/dev.exs
@@ -4,12 +4,14 @@ import Config
 # ENDPOINT
 # =========================
 config :spl, SplWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: String.to_integer(System.get_env("PORT") || "4000")],
+  http: [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
-  secret_key_base: "pR1NF8Rz9D2DSXY721VxCiIOjLkswjanqAMzmUHcoCXwuBY+qDf+Fbv+At52ARHD",
+  secret_key_base: "spl_58@&h84#mb28xjBNABbG945*4%0",
   watchers: []
+
+config :spl, :cors_origins, ["http://localhost:5173", "http://localhost:3000"]
 
 # =========================
 # DEV ROUTES
